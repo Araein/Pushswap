b494ddaabf6a731d92756d0fb8a8f51c688c7fa9
Theo Lebouvier <tlebouvi@student.42.fr>
2021-09-07 16:28:39 +0200

    p

--- listmanagement/allfunc.c
+++ listmanagement/allfunc.c
@@ -15,6 +15,12 @@ struct stack2 {
 };
 typedef struct stack2 stack2_t;
 
+
+
+
+
+
+/* PRINT LIST*/
 void printlist (stack1_t *head)
 {
 	stack1_t *tmp;
@@ -27,23 +33,64 @@ void printlist (stack1_t *head)
 	}
 }
 
+/*CREATE NODE DYNAMICALLY*/
+
+stack1_t *createnode (int value)
+{
+	stack1_t *result = malloc(sizeof(stack1_t));
+	result->value = value;
+	result->next = NULL;
+	return (result);
+}
+
+stack1_t *generatestack(int i)
+{
+
+	stack1_t *head;
+	stack1_t *tmp;
+	int j;
+	
+	head = NULL;
+	j = 0;
+	tmp = createnode(j);
+	while (j < i)
+	{
+		j++;
+		tmp = createnode(j);
+		tmp->next = head;
+		head = tmp;
+	}
+	return (head);
+}
 int main ()
 {
-	stack1_t n1, n2, n3, n4;
-	stack1_t *head;
+/*	stack1_t *head;
+	stack1_t *tmp;
+	int i;
+	
+	head = NULL;
+	i = 0;
+	tmp = createnode(i);
+	while (i < 25)
+	{
+		i++;
+		tmp = createnode(i);
+		tmp->next = head;
+		head = tmp;
+	}
+*/
+/*
 
 	n1.value = 1;
 	n2.value = 2;
 	n3.value = 3;
-	n4.value = 89;
 
 	head = &n1;
-	n1.next = &n4;
-	n4.next = &n2;
+	n1.next = &n2;
 	n2.next = &n3;
 	n3.next = NULL;
-
-	printlist(head);
+*/
+	printlist(generatestack(10));
 
 
 return (0);
