af717c776f84700d6bd054810be4f4f42a8fb4a6
Araein <75018476+Araein@users.noreply.github.com>
2021-09-08 19:46:40 +0200

    Update Pushswap.h

--- header/Pushswap.h
+++ header/Pushswap.h
@@ -1,5 +1,5 @@
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#ifndef PUSHSWAP_H
+# define PUSHSWAP_H
 
 # include <stdio.h>
 # include <stdlib.h>
@@ -7,12 +7,20 @@
 # include <stdarg.h>
 # include <string.h>
 
-typedef struct	s_vari
+typedef struct	stack1
 {
-	int type;
+	int value;
+	struct stack1 *next;
+	struct stack1 *prev;
+}				stack1_t;
 
-}				t_vari;
-
-
+void printlist (stack1_t *head);
+stack1_t *createnode (int value);
+stack1_t *generatestack(int i);
+stack1_t *swapfirst(stack1_t *head);
+void givenode( stack1_t **head,  stack1_t **head2);
+int countelem (stack1_t *head);
+void toptobottom(stack1_t **head);
+void bottomtotop(stack1_t **head);
 
 #endif
