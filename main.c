a1f1fb3a85afdbd91961c8ba359c86d9206fec2e
Araein <75018476+Araein@users.noreply.github.com>
2021-09-08 19:52:16 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,31 @@
-/*wesh*/
+#include "Pushswap.h"
+
+
+int main ()
+{
+	stack1_t *head;
+	stack1_t *head2;
+	stack1_t *head3;
+	head = generatestack(10);
+	
+	head2 = generatestack(10);
+	head3 = generatestack(10);
+
+	bottomtotop(&head2);
+	toptobottom(&head3);
+	givenode(&head, &head2);
+
+	printf("\n--------------- head :\n");
+	printlist(head);
+//	printf("\n head a %d elem", countelem(head));
+	printf("\n--------------- head2 bottomtotop :\n");
+	printlist(head2);
+	printf("\n--------------- head3 toptobottom :\n");
+	printlist(head3);
+
+
+//	printf("\n head2 a %d elem", countelem(head2));
+
+
+	return (0);
+}
