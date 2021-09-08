3966a2b62133b34b9f4984502a4f362362441b73
Araein <75018476+Araein@users.noreply.github.com>
2021-09-08 12:46:23 +0200

    Create Pushswap.h

---
+++ header/Pushswap.h
@@ -0,0 +1,18 @@
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <string.h>
+
+typedef struct	s_vari
+{
+	int type;
+
+}				t_vari;
+
+
+
+#endif
