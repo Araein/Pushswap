4c54f1147c4bded101bd25af172a82c383a02aa9
Theo Lebouvier <tlebouvi@student.42.fr>
2021-10-04 20:38:19 +0200

    mainnormed

--- srcs/main.c
+++ srcs/main.c
@@ -7,7 +7,7 @@ void	ft_putchar(char c)
 
 void	ft_putstr(char const *s)
 {
-	int i;
+	int	i;
 
 	if (s)
 	{
@@ -20,17 +20,17 @@ void	ft_putstr(char const *s)
 	}
 }
 
-long long		ft_atoi(const char *str)
+long long	ft_atoi(const char *str)
 {
-	long long signe;
-	long long i;
-	long long num;
+	long long	signe;
+	long long	i;
+	long long	num;
 
 	i = 0;
 	signe = 1;
 	num = 0;
 	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '\v'
-			|| str[i] == '\f' || str[i] == '\r')
+		|| str[i] == '\f' || str[i] == '\r')
 		i++;
 	if (str[i] == '-' || str[i] == '+')
 	{
@@ -48,22 +48,22 @@ long long		ft_atoi(const char *str)
 
 void	checkdoublon(stack1_t **head, t_stack *stack)
 {
-	int		i;
+	int			i;
 	stack1_t	*tmp;
 	stack1_t	*tmp2;
-	int		d;
+	int			d;
 
 	d = 0;
 	tmp = *head;
 	while (tmp != NULL && stack->error != 1)
 	{	
-		i = tmp->value;	
+		i = tmp->value;
 		tmp2 = *head;
 		d = 0;
 		while (tmp2 != NULL)
 		{
 			if (tmp2->value == i)
-				d++;			
+				d++;
 			tmp2 = tmp2->next;
 		}
 		if (d > 1)
@@ -71,27 +71,67 @@ void	checkdoublon(stack1_t **head, t_stack *stack)
 		tmp = tmp->next;
 	}
 	return ;
-}	
+}
 
+int	littlenum(stack1_t **heada, stack1_t **sorted, t_stack *stack)
+{
+	if (stack->size <= 1)
+	{
+		freelist(heada);
+		freelist(sorted);
+		return (0);
+	}
+	if (stack->size == 2)
+	{
+		if ((*heada)->value > (*heada)->next->value)
+			swapfirst(heada, 0, stack);
+		freelist(heada);
+		freelist(sorted);
+		return (0);
+	}
+	return (0);
+}
 
-int main (int argc, char **argv)
+void	init(stack1_t **heada, stack1_t **headb, t_stack *stack)
+{
+	stack->value = 0;
+	stack->size = 0;
+	stack->therest = 0;
+	stack->sorted = NULL;
+	*headb = NULL;
+	*heada = NULL;
+	stack->operation = 0;
+	stack->one = 11;
+}
+
+int	launchit(stack1_t **ha, stack1_t **hb, stack1_t **sorted, t_stack *stack)
+{
+	if (stack->size == 3)
+		triplechar(ha, stack);
+	else if (stack->size < 6)
+		fivechar(ha, hb, stack->size, stack);
+	else
+	{		
+		sortstack(sorted, stack);
+		if (stack->size < 201)
+			stack->one = 5;
+		dealwithmore(ha, hb, stack);
+	}
+	freelist(sorted);
+	freelist(ha);
+	return (0);
+}
+
+int	main(int argc, char **argv)
 {
 	stack1_t	*heada;
 	stack1_t	*headb;
 	stack1_t	*sorted;
 	t_stack		stack;
 
-
-
-	stack.value = 0;
-	stack.size = 0;
-	stack.therest = 0;
-	stack.sorted = NULL;
-	headb = NULL;
-	heada = NULL;
+	init(&heada, &headb, &stack);
 	if (argv[1] == NULL)
 		return (0);
-
 	stack.error = 0;
 	heada = generatestack(argc, argv, &stack);
 	checkdoublon(&heada, &stack);
@@ -104,34 +144,9 @@ int main (int argc, char **argv)
 	}
 	sorted = generatestack(argc, argv, &stack);
 	stack.size = countelem(heada);
-	if (stack.size <= 1)
-	{
-		freelist(&heada);
-		freelist(&sorted);
-		return (0);
-	}
-	if (stack.size == 2)
-	{
-		if ((*heada).value > (*heada).next->value)
-			swapfirst(&heada, 0, &stack);
-		freelist(&heada);
-                freelist(&sorted);
-		return (0);
-	}
-	stack.operation = 0;
-	stack.one = 11;
-	if (stack.size == 3)
-		triplechar(&heada, &stack);
-	else if (stack.size < 6)
-		fivechar(&heada, &headb, stack.size, &stack);	
+	if (stack.size <= 2)
+		return (littlenum(&heada, &sorted, &stack));
 	else
-	{		
-		sortstack(&sorted, &stack);
-		if (stack.size < 201)
-			stack.one = 5;
-		dealwithmore(&heada, &headb, &stack);	
-	}
-	freelist(&sorted);
-	freelist(&heada);
+		return (launchit(&heada, &headb, &sorted, &stack));
 	return (0);
 }
