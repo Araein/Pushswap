1c8cb4ccc09229a13f2d37ae126898b760be0ae5
Theo Lebouvier <tlebouvi@student.42.fr>
2021-10-06 04:55:59 +0200

    stackednorm

--- srcs/stacked.c
+++ srcs/stacked.c
@@ -1,10 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stacked.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlebouvi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/06 04:11:42 by tlebouvi          #+#    #+#             */
+/*   Updated: 2021/10/06 04:55:22 by tlebouvi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Pushswap.h"
 
+void	swapthem(stack1_t **tmp, int d)
+{
+	int	i;
 
+	if (d == 1 && (*tmp)->value > (*tmp)->next->value)
+	{
+		i = (*tmp)->value;
+		(*tmp)->value = (*tmp)->next->value;
+		(*tmp)->next->value = i;
+	}
+	if (d == 2 && (*tmp)->value > (*tmp)->prev->value)
+	{
+		i = (*tmp)->value;
+		(*tmp)->value = (*tmp)->prev->value;
+		(*tmp)->prev->value = i;
+	}
+}
 
 int	sortstack( stack1_t **head, t_stack *stack)
 {
-	int			i;
 	stack1_t	*tmp;
 	stack1_t	*keep;
 
@@ -12,77 +39,52 @@ int	sortstack( stack1_t **head, t_stack *stack)
 	while (*head != NULL)
 	{
 		tmp = *head;
-        while (tmp->next != NULL)
-        {
-           if (tmp->value > tmp->next->value)
-            {
-              i = tmp->value;
-              tmp->value = tmp->next->value;
-              tmp->next->value = i;
-            }
-         tmp = tmp->next;
-        } 
-        while (tmp->prev != NULL)
-        {
-           if (tmp->value > tmp->prev->value) 
-            {
-              i = tmp->value;
-              tmp->value = tmp->prev->value;
-              tmp->prev->value = i;
-            }
-         tmp = tmp->prev;
-        }
-        *head = (*head)->next;
-    }
+		while (tmp->next != NULL)
+		{
+			swapthem(&tmp, 1);
+			tmp = tmp->next;
+		}
+		while (tmp->prev != NULL)
+		{	
+			swapthem(&tmp, 2);
+			tmp = tmp->prev;
+		}
+		*head = (*head)->next;
+	}
 	*head = keep;
-	
-	//printf("\nsorted = \n");
-	//printlist(*head);
-
 	stack->sorted = *head;
-
 	return (1);
 }
 
-void	findmedian (t_stack *stack)
+void	findmedian( t_stack *stack)
 {
 	stack1_t	*tmp;
 	int			i;
 
-	i = 0;
+	i = -1;
 	tmp = stack->sorted;
-	
-	while (i < stack->size/2)
-	{
+	while (i++ < stack->size / 2)
 		tmp = tmp->next;
-		i++;
-	}
 	stack->median = tmp->value;
-
 	i = 0;
 	tmp = stack->sorted;
-	
-	while (i < stack->size/4)
+	while (i < stack->size / 4)
 	{
 		tmp = tmp->next;
 		i++;
 	}
 	stack->firstquart = tmp->value;
-
 	i = 0;
 	tmp = stack->sorted;
-	
-	while (i < stack->size - stack->size/4)
+	while (i < stack->size - stack->size / 4)
 	{
 		tmp = tmp->next;
 		i++;
 	}
 	stack->thirdquart = tmp->value;
-
-//	freelist(stack->sorted);
 }
 
-void	findhigherlower (stack1_t **head, t_stack *stack)
+void	findhigherlower( stack1_t **head, t_stack *stack)
 {
 	stack1_t	*higher;
 	stack1_t	*lower;
@@ -93,8 +95,6 @@ void	findhigherlower (stack1_t **head, t_stack *stack)
 	lower = *head;
 	while (*head != NULL)
 	{
-	//	if ((*head)->value > higher->value)
-	//		higher = *head;
 		if ((*head)->value > lower->value)
 			lower = *head;
 		*head = (*head)->next;
@@ -104,17 +104,16 @@ void	findhigherlower (stack1_t **head, t_stack *stack)
 	*head = tmp;
 }
 
-int		decidewhofirst(stack1_t **head, t_stack *stack, int i)
+int	decidewhofirst( stack1_t **head, t_stack *stack, int i)
 {
 	stack1_t	*tmp;
 	int			middle;
 	int			j;
 	int			ret;
-	
-	middle = i/2;
+
+	middle = i / 2;
 	j = 0;
 	tmp = *head;
-
 	stack->direction = 1;
 	while (stack->lower->value != (*head)->value)
 	{
@@ -122,129 +121,35 @@ int		decidewhofirst(stack1_t **head, t_stack *stack, int i)
 		*head = (*head)->next;
 	}
 	*head = tmp;
-
-//	printf("\n***lower = %d_ j = %d***\n", stack->lower->value, j);
-                                                          
 	if (j > middle)
 	{
 		j = i - j;
 		stack->direction = 2;
 	}
-		ret = j;
-		return (ret);
+	ret = j;
+	return (ret);
 }
 
 void	flipit( stack1_t **headb, stack1_t **heada, t_stack *stack, int choose)
 {
-	int i;
-//	int d;
+	int	i;
 
-//	d = 0;
 	i = 0;
-	
 	if (stack->direction == 1)
 	{
-	/*	if ((*heada)->value > (*heada)->next->value && choose == 1)
-		{
-//			swapfirst(heada, 2, stack);
-			swapfirst(headb, 0, stack);
-//			printf("ss\n");
-		} else
-	*/	while ( i < choose)
+		while (i < choose)
 		{
 			i++;
-	/*		if ((*heada)->value > (*heada)->next->value)
-			{
-				toptobottom(heada, 2, stack);
-                        	toptobottom(headb, 2, stack);
-                        	printf("rr\n");
-				d++;
-			} else
-			{*/
-				toptobottom (headb, 1, stack);
-	
-			}	
-		}
+			toptobottom (headb, 1, stack);
+		}	
+	}
 	if (stack->direction == 2)
 	{
-		while ( i < choose)
+		while (i < choose)
 		{
 			i++;
-			bottomtotop (headb, 1, stack); //faire un truc pour faire remonter le sbails?
+			bottomtotop (headb, 1, stack);
 		}
 	}
-/*	while ( d != 0)
-	{
-		bottomtotop(heada, 0, stack);
-		d--;
-	}
-*/	givenode( headb, heada, 0, stack);
-}
-
-void	stackedbis(stack1_t **heada, stack1_t **headb, t_stack *stack)
-{
-	int i;
-	int choose;
-	int test;
-
-	test = 0;
-	i = 0;
-	if(stack->size % 2 != 0)
-			test++;
-	while (i < stack->size/2 + test)
-	{
-		if ((*heada)->value <= stack->median)
-		{
-			givenode(heada, headb, 1, stack);
-			i++;
-		} else
-	toptobottom(heada, 0, stack);
-//	printf("coucou i = %d / median= %d / value = %d", i, stack->median, (*heada)->value);
-	}
-	while ((*headb)->next != NULL)
-	{
-		findhigherlower(headb, stack);
-		choose = decidewhofirst(headb, stack, i);
-		flipit(headb, heada, stack, choose);
-		i--;
-	}
-	givenode(headb, heada, 0, stack);
-}
-
-void	stacked(stack1_t **heada, stack1_t **headb, t_stack *stack)
-{
-//	stack1_t	*tmp;
-	int			i;
-	int			choose;
-	int			test;
-
-	test = 0;
-	i = 0;
-//	tmp = *heada;
-	choose = stack->size;
-//	tmp = *headb;
-	choose = 0;
-	
-	//if(stack->size % 2 != 0)
-		test++;
-	while (i < stack->size/2 + test)
-	{
-		if ((*heada)->value >= stack->median)
-		{
-			givenode(heada, headb, 1, stack);
-			i++;
-		} else 
-		toptobottom(heada, 0, stack);
-	//	printf("coucou");
-	}
-	while ((*headb)->next != NULL)
-	{
-		findhigherlower(headb, stack);
-
-		choose = decidewhofirst(headb, stack, i);
-		flipit(headb, heada, stack, choose);
-		i--;
-	}
-	givenode(headb, heada, 0, stack);
-	stackedbis(heada, headb, stack);
+	givenode (headb, heada, 0, stack);
 }
